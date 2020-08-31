c93cf163ee0fd34627b37a30b074ee4bf3d2a6d5
Nishanth-Shanmuganathan <theerannishanth05@gmail.com>
2020-08-31 12:16:37 +0530

    Routing along with components completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,38 @@
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { AchievementsComponent } from './pages/achievements/achievements.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { AcademicsComponent } from './pages/academics/academics.component';
+import { AboutComponent } from './pages/about/about.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: AboutComponent
+  },
+  {
+    path: 'academics',
+    component: AcademicsComponent,
+  },
+  {
+    path: 'skills',
+    component: SkillsComponent,
+  },
+  {
+    path: 'achievements',
+    component: AchievementsComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  }, {
+    path: '**',
+    redirectTo: '/'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './pages/header/header.component';
+import { AboutComponent } from './pages/about/about.component';
+import { AcademicsComponent } from './pages/academics/academics.component';
+import { SkillsComponent } from './pages/skills/skills.component';
+import { AchievementsComponent } from './pages/achievements/achievements.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    AcademicsComponent,
+    SkillsComponent,
+    AchievementsComponent,
+    ProjectsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
